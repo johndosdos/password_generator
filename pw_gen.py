2d3ea47d0ed7d197a26dd2593fdc2e4656a03045
John <128350391+alpaOo@users.noreply.github.com>
2024-06-17 19:56:16 +0800

    Init function for password generation logic

---
+++ pw_gen.py
@@ -0,0 +1,8 @@
+def generate_password(len_password):
+    lower_case = "abcdefghijklmnopqrstuvwxyz"
+    upper_case = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    numbers = "0123456789"
+
+    password_output = ""
+
+
